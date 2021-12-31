3eb19bc3c36dd43884fadaae50b7d0d309712a75
aiotter <git@aiotter.com>
2021-12-31 23:13:27 +0900

    Initial commit

---
+++ mod.ts
@@ -0,0 +1,30 @@
+import { createHtml } from "./template.tsx";
+import { serve } from "https://deno.land/std@0.119.0/http/mod.ts";
+
+// expect "/?url=https://tiktok.com/..."
+serve(async (req: Request) => {
+  const originalUrl = new URL(req.url).searchParams.get("url");
+  if (!originalUrl) return new Response(null, { status: 404 });
+  const oembedUrl = "https://www.tiktok.com/oembed?url=" + originalUrl;
+  return new Response(
+    await createHtml(oembedUrl, originalUrl),
+    {
+      status: 200,
+      headers: new Headers({ "content-type": "html" }),
+    },
+  );
+});
+
+// import { Application, Router, helpers } from "https://deno.land/x/oak@v10.1.0/mod.ts";
+// const app = new Application();
+// const router = new Router();
+//
+// router.get("/", async (ctx) => {
+//   const url = helpers.getQuery(ctx).url;
+//   ctx.response.type = "html";
+//   ctx.response.body = await createHtml(url);
+// });
+//
+// app.use(router.routes());
+// app.use(router.allowedMethods());
+// await app.listen({ port: 8000 });
---
+++ template.tsx
@@ -0,0 +1,54 @@
+/** @jsx Nano.h */
+/** @jsxFrag Nano.Fragment */
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+/// <reference lib="deno.unstable" />
+
+import * as Nano from "https://deno.land/x/nano_jsx@v0.0.26/mod.ts";
+
+type HeadProps = {
+  url: string;
+  title: string;
+  description: string;
+  image: string;
+  redirect: string;
+};
+
+const Head = (props: HeadProps) => (
+  <head>
+    <meta charSet="utf-8" />
+    <link
+      rel="icon"
+      href="https://lf16-tiktok-web.ttwstatic.com/obj/tiktok-web-common-sg/mtact/static/images/logo_144c91a.png"
+    />
+    <meta property="og:title" content={props.title} />
+    <meta property="og:description" content={props.description} />
+    <meta property="og:url" content={props.url} />
+    <meta property="og:image" content={props.image} />
+    <meta property="og:site-name" content="TikTok" />
+
+    <meta name="twitter:card" content="player" />
+    <meta name="twitter:site" content="@tiktok_us" />
+    <meta name="twitter:title" content={props.title} />
+    <meta http-equiv="Refresh" content={`0; url='${props.redirect}'`} />
+  </head>
+);
+
+export async function createHtml(url: string, redirect: string) {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const head = Nano.renderSSR(() => (
+    <Head
+      url={url}
+      title={data.author_name}
+      description={data.title}
+      image={data.thumbnail_url}
+      redirect={redirect}
+    />
+  ));
+
+  return `<html>${head}</html>`;
+}
